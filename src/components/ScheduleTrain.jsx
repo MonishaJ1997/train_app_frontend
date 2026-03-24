64a716f428d40d5e55f3c6dd7921186779f47a56
MonishaJ1997 <monishapython@vtscoders.com>
2026-03-24 14:55:51 +0530

    Update ScheduleTrain.jsx

--- src/components/ScheduleTrain.jsx
+++ src/components/ScheduleTrain.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./ScheduleTrain.css";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://train-app-backend-cdpt.onrender.com";
 
 const ScheduleTrain = () => {
   const [query, setQuery] = useState("");
